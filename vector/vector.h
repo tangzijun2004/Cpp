405a2ef537cf8a9f7646607f9d752cc6bf4e1fe7
tangzijun2004 <872313640@qq.com>
2025-02-23 12:25:44 +0800

    模拟实现vector

---
+++ vector/vector.h
@@ -0,0 +1,242 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<assert.h>
+#include<algorithm>
+using namespace std;
+
+namespace tzj
+{
+	template<class T>
+	class vector
+	{
+	public:
+		typedef T* iterator;
+		typedef const T* const_iterator;
+
+		iterator begin()
+		{
+			return _start;
+		}
+
+		iterator end()
+		{
+			return _finish;
+		}
+
+		const_iterator begin() const
+		{
+			return _start;
+		}
+
+		const_iterator end() const
+		{
+			return _finish;
+		}
+
+		// vector<int> v(10, 1);
+		// vector<int> v(10u, 1);
+		// vector<string> v1(10, "1111"); 
+		vector(size_t n, const T& val = T())
+		{
+			resize(n, val);
+		}
+
+		vector(int n, const T& val = T())
+		{
+			resize(n, val);
+		}
+
+		// [first, last)
+		template<class InputIterator>
+		vector(InputIterator first, InputIterator last)
+		{
+			while (first != last)
+			{
+				push_back(*first);
+				++first;
+			}
+		}
+
+		vector()
+		{}
+
+		vector(const vector<T>& v)
+		{
+			_start = new T[v.capacity()];
+			//memcpy(_start, v._start, sizeof(T)*v.size()); //不能使用，浅拷贝,若T为自定义类型且存在资源管理会导致资源泄露
+			for (size_t i = 0; i < v.size(); i++)
+			{
+				_start[i] = v._start[i];
+			}
+
+			_finish = _start + v.size();
+			_endofstorage = _start + v.capacity();
+		}
+
+		/*vector(const vector<T>& v)
+			:_start(nullptr)
+			, _finish(nullptr)
+			, _endofstorage(nullptr)
+		{
+			reserve(v.capacity());
+			for (auto e : v)
+			{
+				push_back(e);
+			}
+		}*/
+
+		void swap(vector<T>& v)
+		{
+			std::swap(_start, v._start);
+			std::swap(_finish, v._finish);
+			std::swap(_endofstorage, v._endofstorage);
+		}
+
+		// v1 = v2
+		vector<T>& operator=(vector<T> v)
+		{
+			swap(v);
+
+			return *this;
+		}
+
+		~vector()
+		{
+			if (_start)
+			{
+				delete[] _start;
+				_start = _finish = _endofstorage = nullptr;
+			}
+		}
+
+		// 
+		void reserve(size_t n)
+		{
+			if (n > capacity())
+			{
+				size_t sz = size();
+				T* tmp = new T[n];
+				if (_start)
+				{
+					//memcpy(tmp, _start, sizeof(T) * sz);
+					for (size_t i = 0; i < sz; i++)
+					{
+						tmp[i] = _start[i];
+					}
+
+					delete[] _start;
+				}
+
+				_start = tmp;
+				_finish = _start + sz;
+				_endofstorage = _start + n;
+			}
+		}
+
+		void resize(size_t n, const T& val = T())
+		{
+			if (n < size())
+			{
+				_finish = _start + n;
+			}
+			else
+			{
+				reserve(n);
+
+				while (_finish != _start + n)
+				{
+					*_finish = val;
+					++_finish;
+				}
+			}
+		}
+
+		void push_back(const T& x)
+		{
+			/*if (_finish == _endofstorage)
+			{
+				size_t newcapacity = capacity() == 0 ? 4 : capacity() * 2;
+				reserve(newcapacity);
+			}
+			*_finish = x;
+			++_finish;*/
+			insert(end(), x);
+		}
+
+		void pop_back()
+		{
+			erase(--end());
+		}
+
+		size_t capacity() const
+		{
+			return _endofstorage - _start;
+		}
+
+		size_t size() const
+		{
+			return _finish - _start;
+		}
+
+		T& operator[](size_t pos)
+		{
+			assert(pos < size());
+
+			return _start[pos];
+		}
+
+		const T& operator[](size_t pos) const
+		{
+			assert(pos < size());
+
+			return _start[pos];
+		}
+
+		iterator insert(iterator pos, const T& x)
+		{
+			assert(pos >= _start && pos <= _finish);
+
+			if (_finish == _endofstorage)
+			{
+				size_t len = pos - _start;
+
+				size_t newcapacity = capacity() == 0 ? 4 : capacity() * 2;
+				reserve(newcapacity);
+
+				// 解决pos迭代器失效问题
+				pos = _start + len;
+			}
+
+			iterator end = _finish - 1;
+			while (end >= pos)
+			{
+				*(end + 1) = *end;
+				--end;
+			}
+			*pos = x;
+			++_finish;
+
+			return pos;
+		}
+
+		iterator erase(iterator pos)
+		{
+			assert(pos >= _start && pos < _finish);
+
+			iterator it = pos + 1;
+			while (it != _finish)
+			{
+				*(it - 1) = *it;
+				++it;
+			}
+			--_finish;
+
+			return pos;
+		}
+
+	private:
+		iterator _start = nullptr;
+		iterator _finish = nullptr;
+		iterator _endofstorage = nullptr;
+	};
+}
