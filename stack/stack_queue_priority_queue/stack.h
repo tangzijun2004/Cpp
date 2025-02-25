052b8cbf9836451721c42d6ef0bf95302519b3ca
tangzijun2004 <872313640@qq.com>
2025-02-25 09:21:25 +0800

    模拟实现stack

---
+++ stack/stack_queue_priority_queue/stack.h
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+using namespace std;
+
+namespace tzj
+{
+	template<class T, class Container = deque<T>>
+	class stack
+	{
+	public:
+		void push(const T& x)
+		{
+			_con.push_back(x);
+		}
+
+		void pop()
+		{
+			_con.pop_back();
+		}
+
+		size_t Size()
+		{
+			return _con.size();
+		}
+
+		bool empty()
+		{
+			return _con.empty();
+		}
+
+		T& top()
+		{
+			return _con.back();
+		}
+
+	private:
+		Container _con;
+	};
+};
