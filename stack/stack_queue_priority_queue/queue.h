6024bfa39539390c74c8778fe2a37d7ac1bea061
tangzijun2004 <872313640@qq.com>
2025-02-25 09:20:06 +0800

    模拟实现queue

---
+++ stack/stack_queue_priority_queue/queue.h
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+using namespace std;
+#pragma once;
+
+namespace tzj
+{
+	template<class T, class Container=deque<T>>
+	class queue
+	{
+	public:
+		void push(const T& x)
+		{
+			_con.push_back(x);
+		}
+
+		void pop()
+		{
+			_con.pop_front();
+		}
+
+		size_t Size()
+		{
+			return _con.size();
+		}
+
+		bool empty()
+		{
+			return _con.empty();
+		}
+
+		T& front()
+		{
+			return _con.front();
+		}
+
+		T& back()
+		{
+			return _con.back();
+		}
+	private:
+		Container _con;
+	};
+
+}
